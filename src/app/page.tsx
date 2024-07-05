b39defb40c85537d44b883ebf2db673ee900ea44
Seovileoo <96625039+Nawija@users.noreply.github.com>
2024-07-05 15:06:41 +0200

    sdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+import Lottie from "lottie-react";
+import animationData from "../../public/heroAnim.json";
+
 export default function Home() {
     return (
         <div className="wrapper">
-            <section>xxx</section>
+            <section className="py-10 relative flex items-center justify-center">
+                <div>
+                  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ad et modi debitis magnam amet molestiae perspiciatis velit odit</h1>
+
+                </div>
+                <Lottie
+                    animationData={animationData}
+                    className="flex justify-center items-center h-80"
+                    loop={true}
+                />
+            </section>
         </div>
     );
 }
