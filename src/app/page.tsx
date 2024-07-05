d5dd85ee82dd92d7f5fff59914f20ba6be69a4ae
Seovileoo <96625039+Nawija@users.noreply.github.com>
2024-07-05 18:06:35 +0200

    sdfsfdsfd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,40 @@
 "use client";
 import Lottie from "lottie-react";
 import animationData from "../../public/heroAnim.json";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="wrapper">
-            <section className="py-10 relative flex items-center justify-center">
-                <div>
-                  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ad et modi debitis magnam amet molestiae perspiciatis velit odit</h1>
-
+        <div className="">
+            <section className="wrapper py-20 relative flex flex-col lg:flex-row  items-center justify-center">
+                <div className="max-w-screen-sm text-center lg:text-start space-y-8">
+                    <h1 className="text-xl lg:text-4xl font-medium ">
+                        Loredolor sit amet{" "}
+                        <span className="text-accent-primary">consectetur</span>{" "}
+                        adipisicing soluta ad et modi debitis{" "}
+                        <span className="text-accent-primary">magnam</span>
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Dolore sit minus similique cum quaerat, ratione,
+                        qui corporis consequuntur dicta voluptatem quos illo
+                        fuga, corrupti velit.
+                    </p>
+                    <div className="space-x-4 py-4">
+                      <Link href="/" className="py-2 px-4 border rounded-xl border-foreground-primary font-medium">Show info</Link>
+                      <Link href="/" className="py-2 px-4 border rounded-xl border-accent-primary text-accent-primary font-medium">More</Link>
+                    </div>
                 </div>
                 <Lottie
                     animationData={animationData}
-                    className="flex justify-center items-center h-80"
+                    className="flex justify-center items-center"
                     loop={true}
                 />
+            </section>
+            <section className="py-20 bg-background-primary">
+              <div className="wrapper">
+                
+              </div>
             </section>
         </div>
     );
