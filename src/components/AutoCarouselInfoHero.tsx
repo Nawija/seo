0680f3e16abc7b6e283c134466600699f458941d
Valkon <96625039+Nawija@users.noreply.github.com>
2024-07-11 21:31:15 +0200

    fd

--- src/components/AutoCarouselInfoHero.tsx
+++ src/components/AutoCarouselInfoHero.tsx
@@ -1,12 +1,19 @@
-import { NAVIGATION_LINKS } from "@/constants/routers";
+import { NAVIGATION_LINKSS } from "@/constants/routers";
 
 export default function AutoCarouselInfoHero() {
     return (
-        <div className="w-full overflow-hidden p-2 flex items-center justify-start space-x-6 max-w-screen-md mx-auto mt-12 relative">
-            <div className="bg-gradient-to-l from-background-primary via-transparent to-background-primary w-full h-full absolute left-0 top-0"></div>
-            {NAVIGATION_LINKS.map((link, i) => (
-                <p key={i}>{link.name}</p>
-            ))}
+        <div className="w-full overflow-hidden p-2 flex items-center justify-start max-w-screen-md mx-auto mt-12 relative">
+            <div className="bg-gradient-to-l from-background-primary via-transparent to-background-primary w-full h-full absolute left-0 top-0 z-10"></div>
+            <div className="scroll-container">
+                <div className="scroll-content">
+                    {NAVIGATION_LINKSS.map((link, i) => (
+                        <p key={i} className="navigation-link">{link.name}</p>
+                    ))}
+                    {NAVIGATION_LINKSS.map((link, i) => (
+                        <p key={i + NAVIGATION_LINKSS.length} className="navigation-link">{link.name}</p>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
--- src/constants/routers.ts
+++ src/constants/routers.ts
@@ -3,3 +3,17 @@ export const NAVIGATION_LINKS = [
     { name: "about", href: "/about" },
     { name: "contact", href: "/contact" },
 ];
+
+export const NAVIGATION_LINKSS = [
+    { name: "xservices", href: "/services" },
+    { name: "about", href: "/about" },
+    { name: "nextjs", href: "/contact" },
+    { name: "gatsby", href: "/contact" },
+    { name: "hack tools", href: "/contact" },
+    { name: "boty", href: "/contact" },
+    { name: "about", href: "/about" },
+    { name: "nextjs", href: "/contact" },
+    { name: "gatsby", href: "/contact" },
+    { name: "hack tools", href: "/contact" },
+    { name: "jalapenioo", href: "/contact" },
+];
