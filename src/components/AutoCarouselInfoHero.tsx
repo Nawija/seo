09c0fe0458073513e2143037edab5e1a8a75e6a6
Valkon <96625039+Nawija@users.noreply.github.com>
2024-07-11 21:31:47 +0200

    Update AutoCarouselInfoHero.tsx

--- src/components/AutoCarouselInfoHero.tsx
+++ src/components/AutoCarouselInfoHero.tsx
@@ -2,7 +2,7 @@ import { NAVIGATION_LINKSS } from "@/constants/routers";
 
 export default function AutoCarouselInfoHero() {
     return (
-        <div className="w-full overflow-hidden p-2 flex items-center justify-start max-w-screen-md mx-auto mt-12 relative">
+        <div className="w-full overflow-hidden p-2 flex items-center justify-start max-w-screen-sm mx-auto mt-12 relative">
             <div className="bg-gradient-to-l from-background-primary via-transparent to-background-primary w-full h-full absolute left-0 top-0 z-10"></div>
             <div className="scroll-container">
                 <div className="scroll-content">
