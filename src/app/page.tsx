eaab558922bad6ce97c1ab9fa5b7a01e77ae1229
Seovileoo <96625039+Nawija@users.noreply.github.com>
2024-07-12 11:06:53 +0200

    dgf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,23 +7,26 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="anim-opacity space-y-12 pt-12 lg:pt-20">
-            <section className="space-y-6 lg:space-y-12 ">
-                <H1>Lorem ipsum is placeholder Seovileo Commonly</H1>
-                <p className="text-foreground-secondary font text-sm lg:text-lg max-w-screen-md mx-auto text-center px-3">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Aspernatur cumque labore sit accusamus temporibus tenetur,
-                    eius consectetur, esse modi at, corrupti mollitia!
-                </p>
-                <div className="flex items-center justify-center space-x-2">
-                    <MainBtn>Grafika</MainBtn>
-                    <SecondBtn>Wycena</SecondBtn>
+        <div className="anim-opacity pt-12 lg:pt-20">
+            <section className="space-y-12 lg:space-y-20">
+                <div className="space-y-6">
+                    <H1>Lorem ipsum is placeholder Seovileo Commonly</H1>
+                    <p className="text-foreground-secondary font text-sm lg:text-lg max-w-screen-md mx-auto text-center px-3">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Aspernatur cumque labore sit accusamus temporibus
+                        tenetur, eius consectetur, esse modi at, corrupti
+                        mollitia!
+                    </p>
+                    <div className="flex items-center justify-center space-x-2">
+                        <MainBtn>Grafika</MainBtn>
+                        <SecondBtn>Wycena</SecondBtn>
+                    </div>
                 </div>
                 <AutoCarouselInfoHero />
                 <Browser />
             </section>
 
-                <div className="h-[300vh]" />
+            <div className="h-[300vh]" />
         </div>
     );
 }
--- src/components/Browser.tsx
+++ src/components/Browser.tsx
@@ -4,7 +4,7 @@ import { IoIosArrowDown } from "react-icons/io";
 export default function Browser() {
     return (
         <div className="w-[88vw] h-[80vw] max-h-[800px] border border-border-primary wrapper bg-background-secondary rounded-xl backdrop-blur-md relative">
-            <div className="flex items-center justify-between p-2 border-b border-border-primary">
+            <div className="flex items-center justify-between p-2 lg:px-4 border-b border-border-primary">
                 <div className="flex space-x-1 lg:space-x-2 ">
                     <div className=" h-2.5 lg:h-3 w-2.5 lg:w-3 bg-red-500/90 rounded-full" />
                     <div className=" h-2.5 lg:h-3 w-2.5 lg:w-3 bg-yellow-500/90 rounded-full" />
