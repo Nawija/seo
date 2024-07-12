a1e9f7d932ca04e3e1127602104d6ebe830838ae
Seovileoo <96625039+Nawija@users.noreply.github.com>
2024-07-12 10:32:52 +0200

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import Logo from "../Header/Logo";
 export default function Footer() {
     return (
         <footer className="p-10 bg-background-primary border-t border-border-primary">
-            <Logo closeMenu={() => null} h={25} w={25} />
+            <Logo closeMenu={() => null} h={25} w={25} isScrolled={false} />
         </footer>
     );
 }
