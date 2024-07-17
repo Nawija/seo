88d6d8a7515e19b8021eb0a51c7f7f831adeabb3
Valkon <96625039+Nawija@users.noreply.github.com>
2024-07-17 09:08:04 +0200

    fd

--- src/components/AutoCarouselInfoHero.tsx
+++ src/components/AutoCarouselInfoHero.tsx
@@ -2,7 +2,7 @@ import { PROMOTIONAL_TAGS } from "@/constants/promo";
 
 export default function AutoCarouselInfoHero() {
     return (
-        <div className="w-full overflow-hidden p-2 my-6 flex items-center justify-start max-w-screen-sm mx-auto relative">
+        <div className="w-full overflow-hidden p-2 my-6 flex items-center justify-start max-w-4xl mx-auto relative">
             <div className="bg-gradient-to-l from-background-primary via-transparent to-background-primary w-full h-full absolute left-0 top-0 z-10" />
             <div className="scroll-container">
                 <div className="scroll-content">
