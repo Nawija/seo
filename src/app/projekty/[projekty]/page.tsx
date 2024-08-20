35083ea6c40011cd0a8a714b04fe059bed3dde39
Valkon <96625039+Nawija@users.noreply.github.com>
2024-08-20 20:44:20 +0200

    Update page.tsx

--- src/app/projekty/[projekty]/page.tsx
+++ src/app/projekty/[projekty]/page.tsx
@@ -3,8 +3,8 @@ import { PROJECTS } from "@/constants/projects";
 export default function ProjektyPage() {
     return (
         <div className="lg:py-12 py-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-            {PROJECTS.map((project) => (
-                <div className="p-10">
+            {PROJECTS.map((project, index) => (
+                <div key={index} className="p-10">
                     <img src={project.imgUrl} alt="nazoltej" />
                 </div>
             ))}
