82daa06d925cbe9472066c4f7c0bcc03c3f44f79
Seovileoo <96625039+Nawija@users.noreply.github.com>
2024-08-20 10:10:18 +0200

    Update route.ts

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -4,21 +4,21 @@ import bcrypt from "bcryptjs";
 export async function POST(request: Request) {
     const { email, password } = await request.json();
 
-    const user = users.find((user) => user.email === email);
-    if (!user) {
-        return NextResponse.json({
-            success: false,
-            message: "Invalid credentials",
-        });
-    }
+    // const user = users.find((user) => user.email === email);
+    // if (!user) {
+    //     return NextResponse.json({
+    //         success: false,
+    //         message: "Invalid credentials",
+    //     });
+    // }
 
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
-    if (!isPasswordCorrect) {
-        return NextResponse.json({
-            success: false,
-            message: "Invalid credentials",
-        });
-    }
+    // const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    // if (!isPasswordCorrect) {
+    //     return NextResponse.json({
+    //         success: false,
+    //         message: "Invalid credentials",
+    //     });
+    // }
 
     return NextResponse.json({ success: true });
 }
